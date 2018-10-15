74254ac399ea3c1bd135dc42342ef5fbef69ea98
Jack Ling <j.lingyx@gmail.com>
2018-10-15 02:25:27 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -35,4 +35,12 @@ def using_uniq(array)
 end
 
 def using_flatten(array)
-  
+  array.flatten
+end
+
+def using_delete_at(array, int)
+  array.delete()
+end
+
+def using_delete(array, stirng)
+  array.delete(string)
